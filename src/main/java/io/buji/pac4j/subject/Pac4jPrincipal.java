0cce8c96df0b2c7e843697c65738d176f760b04d
polli62 <74900837+polli62@users.noreply.github.com>
2023-11-07 15:43:24 +0100

    Update Pac4jPrincipal.java
    return the principal name as toString() answer

--- src/main/java/io/buji/pac4j/subject/Pac4jPrincipal.java
+++ src/main/java/io/buji/pac4j/subject/Pac4jPrincipal.java
@@ -97,6 +97,11 @@ public class Pac4jPrincipal implements Principal, Serializable {
         return profiles != null ? profiles.hashCode() : 0;
     }
 
+    @Override
+    public String toString() {
+        return getName();
+    }
+
     /**
      * Returns a name for the principal based upon one of the attributes
      * of the main CommonProfile.  The attribute name used to query the CommonProfile 
